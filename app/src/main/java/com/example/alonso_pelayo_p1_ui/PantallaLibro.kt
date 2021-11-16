13921107d2f038c8f9b92eec9adb67e20eb6b1c6
pelayoar <pelayoar@gmail.com>
2021-11-16 20:20:40 +0000

    WIP: anhadido viewBinding e id's

--- app/src/main/java/com/example/alonso_pelayo_p1_ui/PantallaLibro.kt
+++ app/src/main/java/com/example/alonso_pelayo_p1_ui/PantallaLibro.kt
@@ -2,10 +2,30 @@ package com.example.alonso_pelayo_p1_ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.alonso_pelayo_p1_ui.databinding.ActivityPantallaLibroBinding
 
 class PantallaLibro : AppCompatActivity() {
+
+    private lateinit var binding : ActivityPantallaLibroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityPantallaLibroBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pantalla_libro)
+        setContentView(binding.root)
+
+        binding.buttonAceptar.setOnClickListener {
+
+            val autor = binding.textAutor.text
+            val titulo = binding.textTitulo.text
+
+            binding.radioGroupTema.
+
+
+
+        }
+
+
     }
 }
